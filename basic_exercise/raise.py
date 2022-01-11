b1890ae3aeef529f05d92dd3053ae95e4a3f9db3
Stefany <stefanymedina25@gmail.com>
2022-01-11 07:14:32 -0500

    add test with debug error with raise

---
+++ basic_exercise/raise.py
@@ -0,0 +1,15 @@
+# throw errors using the raise keyword. This is helpful when creating your own kinds of exception and error messages.
+
+
+def colorize(text, color):
+    colors = ('blue', 'white', 'red', 'purple')
+    if type(text) is not str:
+        raise TypeError("text must be instance of str")
+    if color not in colors:
+        raise ValueError(f"{color} is invalid color")
+    print(f"Printed {text} in {color}")
+
+colorize("hello", "red") # this is fine
+colorize("hello", "cyan") # this i not good becuse the color is not in the tuple colors
+colorize(123, "red") # this is not good because the first argument is an int and the function is waiting for an string
+
